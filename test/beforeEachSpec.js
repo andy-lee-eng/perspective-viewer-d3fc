8fba1c298f84266a2212c47f70fb07a8de21889d
Matthew Hooper <mhooper@scottlogic.co.uk>
2019-02-19 16:52:56 +0000

    move specs to separate test folder

---
+++ test/beforeEachSpec.js
@@ -0,0 +1,10 @@
+global.CustomEvent = () => {};
+
+global.customElements = {define: () => {}};
+global.HTMLElement = class {
+    getAttribute() {}
+    hasAttribute() {}
+    removeAttribute() {}
+    setAttribute() {}
+};
+
